b5ebfe722f2762e62da8839c4e35a89dc8ce574e
shevaroller <shevaroller@gmail.com>
2014-10-22 18:24:24 -0600

    submission for lab6

--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/IntentReaderActivityTest.java
@@ -16,5 +16,77 @@ public class IntentReaderActivityTest extends
     }
 
 //TODO: Add your code here:
+       public void testSendText() {
+    	   Intent intent = new Intent();
+    	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing send text");
+    	   
+    	   setActivityIntent(intent);
+    	   
+    	   IntentReaderActivity ira = getActivity();
+    	   assertEquals(ira.getText(),"testing send text");
+       }
        
+       public void testDisplayText() {
+    	   Intent intent = new Intent();
+    	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing display text");
+    	   
+    	   setActivityIntent(intent);
+    	   
+    	   IntentReaderActivity ira = getActivity();
+    	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+    	   
+    	   assertEquals(tv.getText(),"testing display text");
+       }
+       
+       public void testDoubleText() {
+    	   Intent intent = new Intent();
+    	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "Bye");
+    	   intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.DOUBLE);
+    	   
+    	   setActivityIntent(intent);
+    	   
+    	   IntentReaderActivity ira = getActivity();
+    	   
+    	   assertEquals(ira.getText(), "ByeBye");
+
+       }
+       
+       public void testReverseText() {
+    	   Intent intent = new Intent();
+    	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "reverse testing");
+    	   intent.putExtra(IntentReaderActivity.MODE_OF_TRANSFORM_KEY, IntentReaderActivity.REVERSE);
+    	   
+    	   setActivityIntent(intent);
+    	   
+    	   IntentReaderActivity ira = getActivity();
+    	   
+    	   assertEquals(ira.getText(), "gnitset esrever");
+    	   
+       }
+       
+       public void testDefaultMessage() {
+    	   Intent intent = new Intent();
+
+    	   String emptyString = null;
+    	   
+    	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, emptyString);
+    	   
+    	   setActivityIntent(intent);
+    	   
+    	   IntentReaderActivity ira = getActivity();
+    	   assertEquals(ira.getText(),"default text");    	   
+       }
+       
+       public void testVisibleText() {
+    	   Intent intent = new Intent();
+    	   intent.putExtra(IntentReaderActivity.TEXT_TO_TRANSFORM_KEY, "testing visible text");
+    	   
+    	   setActivityIntent(intent);
+    	   
+    	   IntentReaderActivity ira = getActivity();
+    	   TextView tv = (TextView)ira.findViewById(ca.ualberta.cs.lonelytwitter.R.id.intentText);
+    	   
+    	   ViewAsserts.assertOnScreen(ira.getWindow().getDecorView(), tv);
+    	   
+       }
 }
--- lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
+++ lonelyTwitterTests/src/ca/ualberta/cs/lonelytwitter/test/LonelyTwitterActivityUITest.java
@@ -36,5 +36,18 @@ public class LonelyTwitterActivityUITest extends
 	}
 	
 //TODO: Add your code here:
+	@UiThreadTest
+	private void testMakeTweet() {
+		LonelyTwitterActivity lta = getActivity();
+		int oldLength = lta.getAdapter().getCount();
+		makeTweet("test new element");
+		ArrayAdapter<NormalTweetModel> aa = lta.getAdapter();
+		assertEquals(aa.getCount(), oldLength+1);
+		
+		assertTrue(aa.getItem(aa.getCount()-1) instanceof NormalTweetModel);
+		
+		assertEquals(aa.getItem(aa.getCount()-1).getText(), "test new element");
+		
+	}
 			
 }
